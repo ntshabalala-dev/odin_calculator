5673ea7d93b10c8c0695b02ce17e0b8736688902
ntshabalala-dev <ntshabalalait@gmail.com>
2025-01-17 11:12:15 +0200

    Add ux functionality

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
 // const plus = document.querySelector('#plus')
 // const clear = document.querySelector('#clear')
-// const input = document.querySelector('input')
+const input = document.querySelector('input')
 
-const buttons = document.querySelector('.buttons');
-let count = 0;
-buttons.addEventListener('click', function name(e) {
+const rightPanel = document.querySelector('.right-panel');
+const numbers = document.querySelector('.left-panel');
+const allRightPanelNodes = document.querySelectorAll('.right-panel button')
 
-    switch (e.target.id) {
-        case 'plus':
-            localStorage.setItem('a', input.value.toString());
-            localStorage.setItem('operator', 'plus');
-            input.value = '';
-            break;
-        case 'equals':
-            const a = localStorage.getItem('a'); //
-            const operator = localStorage.getItem('operator'); //
-            const b = input.value;
+function disableRightPanelNOdes(Nodes) {
+    Nodes.forEach(Node => {
+        Node.toggleAttribute('disabled');
+    });
+}
 
-            console.log(a, b, operator);
+rightPanel.addEventListener('click', function name(e) {
+    if (input.value !== "") {
+        disableRightPanelNOdes(allRightPanelNodes);
+
+        switch (e.target.id) {
+            case 'add':
+                localStorage.setItem('a', input.value.toString());
+                localStorage.setItem('operator', 'plus');
+                input.value = '';
+                break;
+            case 'equals':
+                disableRightPanelNOdes();
+                const a = localStorage.getItem('a'); //
+                const operator = localStorage.getItem('operator'); //
+                const b = input.value;
+
+                console.log(a, b, operator);
 
 
-            switch (operator) {
-                case 'plus':
-                    console.log(+a + +b);
-                    input.value = (+a + +b).toString();
-                    localStorage.clear();
-                    break;
-                default:
-                    break;
-            }
+                switch (operator) {
+                    case 'plus':
+                        console.log(+a + +b);
+                        input.value = (+a + +b).toString();
+                        localStorage.clear();
+                        break;
+                    default:
+                        break;
+                }
 
-            break;
-        case 'clear':
-            // console.log('wtf');
-            localStorage.clear();
-            input.value = '';
-            break;
-        default:
-            break;
+                break;
+            case 'clear':
+                // console.log('wtf');
+                localStorage.clear();
+                input.value = '';
+                break;
+            default:
+                break;
+        }
     }
+
 });
+
+numbers.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.id == 'number') {
+        input.value += target.innerText;
+        console.log(target.innerText);
+    }
+})
