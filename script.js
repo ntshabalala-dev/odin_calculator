cbb39c4fef9dae991b0500f448a8669702492988
ntshabalala-dev <ntshabalalait@gmail.com>
2025-01-24 20:28:34 +0200

    Add additional logic for when other operators are pressed instead of equals

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ const allRightPanelNodes = document.querySelectorAll('.right-panel button')
 let clearInputOnNumberPress = false;
 let appendNumbers = false;
 let isCalculationDone = false;
+let disableRightPanelBUttons = false;
 
 topPanel.addEventListener('click', (e) => {
     const target = e.target;
     let inputValue = input.value;
     const operatorElement = document.getElementById(localStorage.getItem('operator'));
+    document.querySelector('.fourth-row #number').disabled = false;
 
     switch (target.id) {
         case 'clear':
@@ -23,10 +25,12 @@ topPanel.addEventListener('click', (e) => {
 
             if (inputValue !== "") {
                 input.value = '';
-                input.focus()
-                localStorage.clear();
                 appendNumbers = false;
             }
+
+            input.focus()
+            localStorage.clear();
+            // If operators disabled, enable
             break
         case "backspace":
             if (inputValue !== "") {
@@ -34,9 +38,15 @@ topPanel.addEventListener('click', (e) => {
                     operatorElement.classList.toggle('selected');
                     localStorage.removeItem('operator')
                 } else {
+                    // If all digits have been cleared from the screen, clear local storage and enable the decimal bujtton
                     input.value = inputValue.slice(0, inputValue.length - 1);
                     if (input.value == '') {
+                        input.focus()
+                        localStorage.clear();
+
                         localStorage.clear()
+                        document.querySelector('.fourth-row #number').disabled = false;
+                        // If operators disabled, enable
                     }
                 }
             }
@@ -47,7 +57,9 @@ topPanel.addEventListener('click', (e) => {
 })
 
 function setOperandAndOperator(operand, operator) {
+
     if (!localStorage.getItem('a')) {
+        // Set operator and operand if operand a is not set
         localStorage.setItem('a', operand);
         localStorage.setItem('operator', operator);
     } else {
@@ -60,6 +72,9 @@ function setOperandAndOperator(operand, operator) {
 
 rightPanel.addEventListener('click', function name(e) {
     const operator = e.target.id
+    const operatorStorage = localStorage.getItem('operator');
+    const num = input.value;
+    const operatorElement = document.getElementById(operatorStorage);
 
     if (operator == 'subtract' && (
         localStorage.getItem('operator') == 'subtract' ||
@@ -76,22 +91,37 @@ rightPanel.addEventListener('click', function name(e) {
             operatorElement.classList.toggle('selected');
         }
     } else {
-        const iV = input.value
+        // If any of the operator signs are clicked b4 equals, simulate equals
+        if (operatorStorage && num !== "" && num !== localStorage.getItem('a')) {
+            // Highlight new operator
+            if (operatorElement && operatorElement.classList.contains('selected')) {
+                operatorElement.classList.toggle('selected');
+                document.getElementById(operator).classList.toggle('selected');
+            }
 
-        console.log(Number.isInteger(+iV));
+            // Simulate the equals button
+            input.value = equals(num);
 
-        if (iV == "" || !Number.isInteger(+iV)) {
-            return;
+            // Set new operator and continue with calculation
+            localStorage.setItem('a', input.value);
+            localStorage.setItem('operator', operator);
+
+            // disable operators
+        } else {
+            if (num == "" || (!Number.isInteger(+num) && !isFloat(num))) {
+                return;
+            }
+
+            if (operatorElement && operatorElement.classList.contains('selected')) {
+                operatorElement.classList.toggle('selected');
+                localStorage.removeItem('operator')
+            }
+
+            document.getElementById(operator).classList.toggle('selected');
+            setOperandAndOperator(num, operator);
+            //disableRightPanelBUttons = true;
         }
 
-        const operatorElement = document.getElementById(localStorage.getItem('operator'));
-        if (operatorElement && operatorElement.classList.contains('selected')) {
-            operatorElement.classList.toggle('selected');
-            localStorage.removeItem('operator')
-        }
-
-        document.getElementById(operator).classList.toggle('selected');
-        setOperandAndOperator(iV, operator)
     }
 });
 
@@ -120,44 +150,13 @@ leftPanel.addEventListener('click', (e) => {
                 target.disabled = true;
             }
 
+            // const operator = localStorage.getItem('operator');
+            // console.log(operator);
+
+
             break;
         case 'equals':
-            if (input.value == '' || !localStorage.getItem('a')) {
-                return;
-            }
-            appendNumbers = false;
-            clearInputOnNumberPress = true;
-            isCalculationDone = true;
-
-            document.querySelector('.fourth-row #number').disabled = false;
-
-            const a = localStorage.getItem('a');
-            const operator = localStorage.getItem('operator');
-            const b = input.value;
-            let result = null
-            const operatorElement = document.getElementById(operator);
-            if (operatorElement.classList.contains('selected')) {
-                operatorElement.classList.toggle('selected');
-            }
-
-            switch (operator) {
-                case 'add':
-                    result = (+a + +b).toString();
-                    break;
-                case 'subtract':
-                    result = (+a - +b).toString();
-                    break;
-                case 'multiply':
-                    result = (+a * +b).toString();
-                    break;
-                case 'divide':
-                    result = (+a / +b).toString();
-                    break;
-                default:
-                    break;
-            }
-
-            input.value = result;
+            input.value = equals(input.value);
             input.focus();
             localStorage.clear();
             break
@@ -171,3 +170,52 @@ window.addEventListener('unload', function () {
     // Clear local storage when the page is closed or refreshed
     localStorage.clear();
 });
+
+function isFloat(value) {
+    const floatVal = parseFloat(value)
+    return typeof floatVal === 'number' && !Number.isInteger(floatVal);
+}
+
+function equals(number) {
+    if (number == '' || !localStorage.getItem('a')) {
+        return '';
+    }
+    appendNumbers = false;
+    clearInputOnNumberPress = true;
+    isCalculationDone = true;
+
+    document.querySelector('.fourth-row #number').disabled = false;
+
+    const a = localStorage.getItem('a');
+    const operator = localStorage.getItem('operator');
+    const b = number;
+
+    let result = null
+    const operatorElement2 = document.getElementById(operator);
+
+    if (operatorElement2.classList.contains('selected')) {
+        operatorElement2.classList.toggle('selected');
+    }
+
+    switch (operator) {
+        case 'add':
+            result = (+a + +b).toString();
+            break;
+        case 'subtract':
+            result = (+a - +b).toString();
+            break;
+        case 'multiply':
+            result = (+a * +b).toString();
+            break;
+        case 'divide':
+            result = (+a / +b).toString();
+            break;
+        default:
+            break;
+    }
+
+    // If operators disabled, enable
+
+    console.log(result);
+    return result;
+}
